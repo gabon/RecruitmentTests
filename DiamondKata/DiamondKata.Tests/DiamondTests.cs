409fbef425038ca4d21e88acbd3ff246263b8a7c
Gabriel Mazur <gabriel.mazur@softwareone.com>
2024-02-22 11:40:00 +0100

    Adding test cases and cleanup demo tests

--- DiamondKata/DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata/DiamondKata.Tests/DiamondTests.cs
@@ -1,12 +1,62 @@
+using Xunit.Abstractions;
+
 namespace DiamondKata.Tests;
 
 public class DiamondTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public DiamondTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    public static IEnumerable<object[]> DiamondTestData()
+    {
+        yield return new object[]
+        {
+            'A', new[,]
+            {
+                { 'A' }
+            }
+        };
+        yield return new object[]
+        {
+            'B', new[,]
+            {
+                { ' ', 'A', ' ' },
+                { 'B', ' ', 'B' },
+                { ' ', 'A', ' ' }
+            }
+        };
+        yield return new object[]
+        {
+            'C',
+            new[,]
+            {
+                { ' ', ' ', 'A', ' ', ' ' },
+                { ' ', 'B', ' ', 'B', ' ' },
+                { 'C', ' ', ' ', ' ', 'C' },
+                { ' ', 'B', ' ', 'B', ' ' },
+                { ' ', ' ', 'A', ' ', ' ' }
+            }
+        };
+    }
+
     [Fact]
     public void Test1()
     {
-        Assert.Equal(new[,] { { 'A' } },
-            Diamond.Generate('A'));
+        // Arrange
+        var letter = 'A';
+        var expected = new[,] { { 'A' } };
+
+        // Act
+        char[,] result = Diamond.Generate(letter);
+
+        // Assert
+        _output.WriteLine(result.ToMultilineString());
+        Assert.Equal(expected,
+            result);
     }
 }
 
