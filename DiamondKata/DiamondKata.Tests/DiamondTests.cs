e4f3460aa8f0e42b74dc7820b2b93b606912659e
Gabriel Mazur <gabriel.mazur@softwareone.com>
2024-02-22 11:18:59 +0100

    Adding extension methods for output

--- DiamondKata/DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata/DiamondKata.Tests/DiamondTests.cs
@@ -5,15 +5,40 @@ public class DiamondTests
     [Fact]
     public void Test1()
     {
-        Assert.Equal("A",
+        Assert.Equal(new[,] { { 'A' } },
             Diamond.Generate('A'));
     }
 }
 
+public class MatrixExtensionTests
+{
+    [Fact]
+    public void ToMultilineString_OneLetter_OneLetter()
+    {
+        var matrix = new[,] { { 'A' } };
+        Assert.Equal("A", matrix.ToString());
+    }
+    
+    [Fact]
+    public void ToMultilineString_TwoOnOne_ConcatenatedOneLine()
+    {
+        var matrix = new[,] { { 'A', 'B' } };
+        Assert.Equal("A B", matrix.ToString());
+    }
+    
+    [Fact]
+    public void ToMultilineString_TwoOnTwo_ConcatenatedTwoLines()
+    {
+        var matrix = new[,] { { 'A', 'B' }, { 'C', 'D' } };
+        Assert.Equal("A B\nC D", matrix.ToString());
+    }
+ 
+}
+
 public class Diamond
 {
-    public static string Generate(char letter)
+    public static char[,] Generate(char letter)
     {
-        return "A";
+        return new[,] { { 'A' } };
     }
 }
