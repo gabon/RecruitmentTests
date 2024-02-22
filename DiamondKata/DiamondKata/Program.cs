a231d8092674a4c522d52606492a170e5e7ccaca
Gabriel Mazur <gabriel.mazur@softwareone.com>
2024-02-22 12:39:31 +0100

    moving files and attaching to console app

--- DiamondKata/DiamondKata/Program.cs
+++ DiamondKata/DiamondKata/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DiamondKata;
+
+if (args.Length != 1 || args[0].Length != 1)
+{
+    Console.WriteLine("Please enter a single character.");
+    return;
+}
+
+try
+{
+    char letter = args[0][0];
+    char[,] result = Diamond.Generate(letter);
+    Console.WriteLine(result.ToMultilineString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
